2b80f789f40674820c0037d82e0a7cf047b66aa5
rocket3989 <rocket3989@gmail.com>
2019-09-17 19:46:39 -0500

    update leaders

--- Practice Solutions/9-17-19/leaders.py
+++ Practice Solutions/9-17-19/leaders.py
@@ -1,14 +1,16 @@
+import sys
+sys.stdin = open("A-large-practice.in") # use this to read in a file
 for tc in range(1,int(input()) + 1):
     max = 0
     maxName = ""
     for i in range(int(input())):
         stringIn = input()
         setChars = set()
-        for c in stringIn.strip():
+        for c in stringIn.replace(' ', ''):
             setChars.add(c)
         if len(setChars) == max:
             maxName = stringIn if stringIn < maxName else maxName
         if len(setChars) > max:
             max = len(setChars)
             maxName = stringIn
-    print("Case#", tc, ": ", maxName, sep='')
+    print("Case #", tc, ": ", maxName, sep='')
